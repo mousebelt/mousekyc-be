f9dcc349a2e7bf8d67f0f6fdf7f26749bc75ccdf
Ohta <ohtayoshida8912@yahoo.com>
2018-06-11 01:48:32 -0700

    updated user model and update api

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -21,7 +21,7 @@ const MailService = require('../services/mail.service');
 */
 exports.getInfoToken = async (req, res) => {
   var token = String(req.params.token);
-  
+
   try {
     var userRow = await UserModel.findOne({ token });
     if (!userRow) return res.json({ status: 400, msg: "Invalid token !" });
@@ -64,6 +64,218 @@ exports.postGenToken = async (req, res) => {
   res.json({ status: 200, msg: 'success', data: { token, email } });
 }
 
+/**
+ * @function: Update user from token, email
+ * 
+ * @method: POST /update
+ * 
+ * @param {String|Required} email
+ * @param {String|Required} token
+ * 
+ * @param {String} firstname
+ * @param {String} lastname
+ * @param {String} dob
+ * @param {String} documentExpireDate
+ * @param {String} nationalityCountry
+ * @param {String} documentId
+//  * @param {String} documentType
+//  * @param {String} identityDocument
+ * 
+ * @param {String} phone
+ * @param {String} residenceCountry
+ * @param {String} residenceAddress
+//  * @param {String} selfie
+ * 
+ * @param {String} adminContact
+ * @param {String} checkStatus
+ * @param {String} adminMessage
+ * @param {String} backgroundCheckId
+ * 
+ * @return
+ * { "status": 200, "msg": "success", data: userInfo }
+*/
+exports.postUpdate = async (req, res) => {
+  const gfs = req.app.get("gfs");
+  var {
+    email,
+    token,
+    //
+    firstname,
+    lastname,
+    dob,
+    documentExpireDate,
+    nationalityCountry,
+    documentId,
+    //
+    phone,
+    residenceCountry,
+    residenceAddress,
+    //
+    adminContact,
+    checkStatus,
+    adminMessage,
+    backgroundCheckId
+  } = req.body;
+
+  if (!UtilsModule.validateEmail(email)) res.json({ status: 400, msg: "Invalid email !" });
+  if (!token || token == '') res.json({ status: 400, msg: "Empty token !" });
+
+  try {
+    var userRow = await UserModel.findOne({ email });
+    if (!userRow) return res.json({ status: 400, msg: "No existing user !" });
+    if (userRow.token != token) return res.json({ status: 400, msg: "Invalid token !" });
+
+    // Add user
+    userRow.set({
+      firstname: firstname || userRow.firstname,
+      lastname: lastname || userRow.lastname,
+      //
+      dob: dob || userRow.dob,
+      documentExpireDate: documentExpireDate || userRow.documentExpireDate,
+      nationalityCountry: nationalityCountry || userRow.nationalityCountry,
+      documentId: documentId || userRow.documentId,
+      //
+      phone: phone || userRow.phone,
+      residenceCountry: residenceCountry || userRow.residenceCountry,
+      residenceAddress: residenceAddress || userRow.residenceAddress,
+      //
+      adminContact: adminContact || userRow.adminContact,
+      // checkStatus: checkStatus || userRow.checkStatus,
+      adminMessage: adminMessage || userRow.adminMessage,
+      backgroundCheckId: backgroundCheckId || userRow.backgroundCheckId,
+    });
+
+    userRow.save(err => {
+      if (err) {
+        return res.json({ status: 400, msg: "User save error !", data: err });
+      }
+      return res.json({ status: 200, msg: "success", data: userRow });
+    });
+  } catch (error) {
+    return res.json({ status: 400, msg: "DB is not working !", data: error });
+  }
+};
+
+// exports.postUpdate = async (req, res) => {
+//   const gfs = req.app.get("gfs");
+//   var {
+//     email,
+//     token,
+//     name,
+//     phone,
+//     dob,
+//     nationalityCountry,
+//     residenceCountry,
+//     residenceAddress,
+//     adminContact,
+//     checkStatus,
+//     adminMessage,
+//     backgroundCheckId
+//   } = req.body;
+
+//   if (!email || email == '') res.json({ status: 400, msg: "Empty email !" });
+//   if (!token || token == '') res.json({ status: 400, msg: "Empty token !" });
+
+//   let identityDocument, selfie;
+//   try {
+//     identityDocument = req.files.identityDocument;
+//     selfie = req.files.selfie;
+//   } catch (errors) { }
+
+//   // validation
+//   if (!UtilsModule.validateEmail(email))
+//     return res.json({ status: 400, msg: "Invalid email !" });
+
+//   try {
+//     var userRow = await UserModel.findOne({ email });
+//     if (!userRow) return res.json({ status: 400, msg: "No existing user !" });
+//     if (userRow.token != token) return res.json({ status: 400, msg: "Invalid token !" });
+
+//     // Save identityDocument to gfs
+//     var identityDocumentDbFile = undefined;
+//     if (identityDocument) {
+//       var filename =
+//         `identityDocument` +
+//         "-" +
+//         Date.now() +
+//         "." +
+//         identityDocument.name.split(".")[
+//         identityDocument.name.split(".").length - 1
+//         ];
+//       var filepath = "./uploads/" + filename;
+//       var err = await identityDocument.mv(filepath);
+
+//       if (!err && fs.existsSync(filepath)) {
+//         try {
+//           var writestream = gfs.createWriteStream({ filename });
+//           fs
+//             .createReadStream(filepath)
+//             // .on("end", function() {
+//             //   fs.unlink(filepath, function(err) {
+//             //     if (err) console.log("unlink error: ", err);
+//             //   });
+//             // })
+//             .pipe(writestream);
+//           identityDocumentDbFile = filename;
+//         } catch (error) { }
+//       }
+//     }
+
+//     // Save selfie to gfs
+//     var selfieDbFile = undefined;
+//     if (selfie) {
+//       var filename =
+//         `selfie` +
+//         "-" +
+//         Date.now() +
+//         "." +
+//         selfie.name.split(".")[selfie.name.split(".").length - 1];
+//       var filepath = "./uploads/" + filename;
+//       var err = await selfie.mv(filepath);
+
+//       if (!err && fs.existsSync(filepath)) {
+//         try {
+//           var writestream = gfs.createWriteStream({ filename });
+//           fs
+//             .createReadStream(filepath)
+//             // .on("end", function() {
+//             //   fs.unlink(filepath, function(err) {
+//             //     if (err) console.log("unlink error: ", err);
+//             //   });
+//             // })
+//             .pipe(writestream);
+//           selfieDbFile = filename;
+//         } catch (error) { }
+//       }
+//     }
+
+//     // Add user
+//     userRow.set({
+//       name: name || userRow.name,
+//       // email: email || userRow.email,
+//       phone: phone || userRow.phone,
+//       dob: dob || userRow.dob,
+//       nationalityCountry: nationalityCountry || userRow.nationalityCountry,
+//       residenceCountry: residenceCountry || userRow.residenceCountry,
+//       residenceAddress: residenceAddress || userRow.residenceAddress,
+//       identityDocument: identityDocumentDbFile || userRow.identityDocument,
+//       selfie: selfieDbFile || userRow.selfie,
+//       adminContact: adminContact || userRow.adminContact,
+//       checkStatus: checkStatus || userRow.checkStatus,
+//       adminMessage: adminMessage || userRow.adminMessage,
+//       backgroundCheckId: backgroundCheckId || userRow.backgroundCheckId
+//     });
+
+//     userRow.save(err => {
+//       if (err) {
+//         return res.json({ status: 400, msg: "User save error !", data: err });
+//       }
+//       return res.json({ status: 200, msg: "success", data: userRow });
+//     });
+//   } catch (error) {
+//     return res.json({ status: 400, msg: "DB is not working !", data: error });
+//   }
+// };
 // exports.postAdd = async (req, res) => {
 //   var email = req.body.email;
 //   var token = req.body.token;
@@ -203,148 +415,3 @@ exports.postGenToken = async (req, res) => {
 //     return res.json({ status: 400, msg: "DB is not working !", data: error });
 //   }
 // };
-
-/**
- * @function: Update user from token, email
- * 
- * @method: POST /update
- * 
- * @param {String|Required} email
- * @param {String|Required} token
- * @param {String} name
- * @param {String} phone
- * @param {String} dob
- * @param {String} nationalityCountry
- * @param {String} residenceCountry
- * @param {String} residenceAddress
- * @param {String} adminContact
- * @param {String} checkStatus
- * @param {String} adminMessage
- * @param {String} backgroundCheckId
- * 
- * @param {file} identityDocument
- * @param {file} selfie
- * 
- * @return
- * { "status": 200, "msg": "success", data: userInfo }
-*/
-exports.postUpdate = async (req, res) => {
-  const gfs = req.app.get("gfs");
-  var {
-    email,
-    token,
-    name,
-    phone,
-    dob,
-    nationalityCountry,
-    residenceCountry,
-    residenceAddress,
-    adminContact,
-    checkStatus,
-    adminMessage,
-    backgroundCheckId
-  } = req.body;
-
-  if (!email || email == '') res.json({ status: 400, msg: "Empty email !" });
-  if (!token || token == '') res.json({ status: 400, msg: "Empty token !" });
-
-  let identityDocument, selfie;
-  try {
-    identityDocument = req.files.identityDocument;
-    selfie = req.files.selfie;
-  } catch (errors) { }
-
-  // validation
-  if (!UtilsModule.validateEmail(email))
-    return res.json({ status: 400, msg: "Invalid email !" });
-
-  try {
-    var userRow = await UserModel.findOne({ email });
-    if (!userRow) return res.json({ status: 400, msg: "No existing user !" });
-    if (userRow.token != token) return res.json({ status: 400, msg: "Invalid token !" });
-
-    // Save identityDocument to gfs
-    var identityDocumentDbFile = undefined;
-    if (identityDocument) {
-      var filename =
-        `identityDocument` +
-        "-" +
-        Date.now() +
-        "." +
-        identityDocument.name.split(".")[
-        identityDocument.name.split(".").length - 1
-        ];
-      var filepath = "./uploads/" + filename;
-      var err = await identityDocument.mv(filepath);
-
-      if (!err && fs.existsSync(filepath)) {
-        try {
-          var writestream = gfs.createWriteStream({ filename });
-          fs
-            .createReadStream(filepath)
-            // .on("end", function() {
-            //   fs.unlink(filepath, function(err) {
-            //     if (err) console.log("unlink error: ", err);
-            //   });
-            // })
-            .pipe(writestream);
-          identityDocumentDbFile = filename;
-        } catch (error) { }
-      }
-    }
-
-    // Save selfie to gfs
-    var selfieDbFile = undefined;
-    if (selfie) {
-      var filename =
-        `selfie` +
-        "-" +
-        Date.now() +
-        "." +
-        selfie.name.split(".")[selfie.name.split(".").length - 1];
-      var filepath = "./uploads/" + filename;
-      var err = await selfie.mv(filepath);
-
-      if (!err && fs.existsSync(filepath)) {
-        try {
-          var writestream = gfs.createWriteStream({ filename });
-          fs
-            .createReadStream(filepath)
-            // .on("end", function() {
-            //   fs.unlink(filepath, function(err) {
-            //     if (err) console.log("unlink error: ", err);
-            //   });
-            // })
-            .pipe(writestream);
-          selfieDbFile = filename;
-        } catch (error) { }
-      }
-    }
-
-    // Add user
-    userRow.set({
-      name: name || userRow.name,
-      // email: email || userRow.email,
-      phone: phone || userRow.phone,
-      dob: dob || userRow.dob,
-      nationalityCountry: nationalityCountry || userRow.nationalityCountry,
-      residenceCountry: residenceCountry || userRow.residenceCountry,
-      residenceAddress: residenceAddress || userRow.residenceAddress,
-      identityDocument: identityDocumentDbFile || userRow.identityDocument,
-      selfie: selfieDbFile || userRow.selfie,
-      adminContact: adminContact || userRow.adminContact,
-      checkStatus: checkStatus || userRow.checkStatus,
-      adminMessage: adminMessage || userRow.adminMessage,
-      backgroundCheckId: backgroundCheckId || userRow.backgroundCheckId
-    });
-
-    userRow.save(err => {
-      if (err) {
-        return res.json({ status: 400, msg: "User save error !", data: err });
-      }
-      return res.json({ status: 200, msg: "success", data: userRow });
-    });
-  } catch (error) {
-    return res.json({ status: 400, msg: "DB is not working !", data: error });
-  }
-};
--- server/models/user.js
+++ server/models/user.js
@@ -5,24 +5,36 @@ var Schema = mongoose.Schema;
 // create a schema
 var userSchema = new Schema(
   {
-    name: String,
     email: { type: String, unique: true },
     token: String,
     tokenExpireDate: Date,
-  
-    phone: String,
+
+    // passport/id card infomation
+    firstname: String,
+    lastname: String,
     dob: Date,
+    documentExpireDate: Date,
     nationalityCountry: String, // Country code
+    documentId: String,
+    documentType: {
+      type: String,
+      enum: ['PASSPORT', 'IDCARD'],
+      default: "PASSPORT"
+    },
+    identityDocument: String, // photo in blobstore
+
+    phone: String,
     residenceCountry: String, // Country code
     residenceAddress: String,
-    identityDocument: String, // photo in blobstore
     selfie: String, // photo in blobstore
+
     approvalStatus: {
       type: String,
       // enum: ['NO_SUBMISSION_YET', PENDING', 'APPROVED', 'ACTION_REQUESTED', 'BLOCKED'],
       default: "NO_SUBMISSION_YET"
     },
     approvalDescription: String, // ex: "Please submit a clearer selfie, identity is not clear from photo"
+
     adminContact: String,
     checkStatus: String,
     adminMessage: String,
