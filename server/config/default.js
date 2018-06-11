4453e3e822e097df4d9e4c8a25f517c2da6170cb
Ohta <ohtayoshida8912@yahoo.com>
2018-06-11 14:04:46 -0700

    add mailgun service

--- server/config/default.js
+++ server/config/default.js
@@ -3,10 +3,10 @@ const config = {
   db: 'mongodb://mongo:27017/kyc-db',
   // db: 'mongodb://localhost:27017/kyc-db',
   email: {
-    // domain: MAILGUN_DOMAIN || 'icodashboard.space',
-    // mailgun: {
-    //   secret: MAILGUN_API_KEY || 'key-0123456789'
-    // },
+    domain: 'icodashboard.space',
+    mailgun: {
+      secret: 'key-0123456789'
+    },
     // mailjet: {
     //   apiKey: MAILJET_API_KEY,
     //   apiSecret: MAILJET_API_SECRET
--- server/services/mail.service.js
+++ server/services/mail.service.js
@@ -1,20 +1,56 @@
 const nodemailer = require("nodemailer");
 const config = require("../config");
+const mailcomposer = require("mailcomposer");
+ 
+exports.send = (from, to, subject, text) => {
+  const mailgun = require('mailgun-js')({apiKey: config.email.mailgun.scret, domain: config.email.domain});
+  // const mailOptions = {
+  //   from,
+  //   to,
+  //   subject,
+  //   text
+  // };
+  // mailgun.messages().send(data, function (error, body) {
+  //   console.log(body);
+  // });
+  return new Promise((resolve, reject) => {
+      let mail = mailcomposer({
+        from,
+        to,
+        subject,
+        html: text
+      });
 
-exports.send = (sender, recipient, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    service: "SendGrid",
-    auth: {
-      user: config.email.sendgrid.USER,
-      pass: config.email.sendgrid.PASS
-    }
-  });
-  const mailOptions = {
-    to: recipient,
-    from: sender,
-    subject,
-    text
-  };
-  return transporter
-    .sendMail(mailOptions);
+      mail.build((mailBuildError, message) => {
+        let dataToSend = {
+          to: recipient,
+          message: message.toString('ascii')
+        };
+
+        mailgun.messages().sendMime(dataToSend, (err, body) => {
+          if (err) {
+            // this.logger.exception(err);
+            reject(new Error(err));
+          }
+          resolve(body);
+        });
+      });
+    });
 };
+// exports.send = (sender, recipient, subject, text) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "SendGrid",
+//     auth: {
+//       user: config.email.sendgrid.USER,
+//       pass: config.email.sendgrid.PASS
+//     }
+//   });
+//   const mailOptions = {
+//     to: recipient,
+//     from: sender,
+//     subject,
+//     text
+//   };
+//   return transporter
+//     .sendMail(mailOptions);
+// };
