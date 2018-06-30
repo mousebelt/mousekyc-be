49a83c74efbf1042d1eca94c9e7c78a515fe10c1
Galen Danziger <galen@Galens-Mac-mini.local>
2018-06-29 22:43:30 -0700

    Standalone Passport Vision Example to Integrate

---
+++ passport-vision/README.md
@@ -0,0 +1,10 @@
+Install (from ubuntu16.04):
+
+```
+apt-get update && apt-get install -y software-properties-common && add-apt-repository -y ppa:alex-p/tesseract-ocr
+apt-get update && apt-get install -y tesseract-ocr-all 
+apt-get install python-pip
+pip install PassportEye
+```
+
+ß
---
+++ passport-vision/vision.js
@@ -0,0 +1,68 @@
+var moment = require('moment');
+const Queue = require('bee-queue');
+
+
+const express = require('express');
+const app = express()
+
+const identityDocumentQueue = new Queue('identityDocument')
+
+//some constants
+const STATUS_VALID = "valid";
+const STATUS_INVALID = "invalid";
+const STATUS_ERROR = "error";
+
+
+app.get('/', (req, res) => {
+    const documentJob = identityDocumentQueue.createJob({ userId: "1342132154132", documentKey: "passport1.jpg" })
+    documentJob.save();
+    res.send('Job added!')
+}
+)
+
+app.listen(8888, () => console.log('Passport worker listening on port 8888!'))
+
+
+identityDocumentQueue.process(function (job, done) {
+    console.log(`[CV] Processing job ${job.id} - Processing file ${job.data.documentKey} for ${job.data.userId}.`);
+
+    // Here is where we would fetch the data for the file from the database.
+    // I am passing in a documentKey of a file locally for now.
+    // We should either read the file in from gridFS directly, or save to a tmp file in the directory.
+    // I am mocking with the filePath parameter.
+    var filePath = job.data.documentKey;
+    
+    var stdout = require('child_process').execSync(`mrz --json ${filePath}`);
+    console.log(`[CV] Processing Task Completed.`)
+    console.log(stdout);
+    var matchedPassport = JSON.parse(stdout)
+    if  (matchedPassport.valid_score > 50) {
+        console.log("[CV] Valid document detected.")
+        console.log(matchedPassport);
+        var match = {};
+        match.processed = new Date();
+        match.firstname = matchedPassport.names;
+        match.lastname = matchedPassport.surname;
+        //match.gender = matchedPassport.sex;
+        if(matchedPassport.valid_date_of_birth) match.dob = moment(matchedPassport.date_of_birth, "YYMMDD").toDate();
+        if(matchedPassport.valid_expiration_date) match.documentExpireDate = moment(matchedPassport.expiration_date, "YYMMDD").toDate();
+        match.documentId = matchedPassport.number;
+        console.log(match);
+
+        // We should update MongoDB and add a record match with the match object
+        // Expose this via API to the frontend and allow users to compare based on matched fields.
+        // Our Schema is:
+        // processed (timestamp of process) - Date
+        // firstname (first name of user) - String
+        // lastname (last name of user) - String
+        // documentId (id on document) - String
+        // Date of Birth -- String
+        // Date of expiration  -- String
+        // Gender (not supported now, leave out. Kept in object for option of future support.)
+        return done(null, STATUS_SUCCESS);
+    } else {
+        console.log("[CV] No valid document detected.")
+        return done(null, STATUS_INVALID);
+    }
+
+});
