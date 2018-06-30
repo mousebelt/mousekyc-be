0f51d452daaa4db8ec3e133523762d88aff87f75
Galen Danziger <galen@Galens-Mac-mini.local>
2018-06-29 22:59:46 -0700

    Readme minus extra configs

--- passport-vision/README.md
+++ passport-vision/README.md
@@ -3,8 +3,22 @@ Install (from ubuntu16.04):
 ```
 apt-get update && apt-get install -y software-properties-common && add-apt-repository -y ppa:alex-p/tesseract-ocr
 apt-get update && apt-get install -y tesseract-ocr-all 
+apt-get install redis-server
 apt-get install python-pip
 pip install PassportEye
 ```
 
-ß
+for Demo app:
+```
+npm install bee-queue
+npm install moment
+node server.js
+```
+
+Test:
+
+```
+curl localhost:8888
+```
+
+call repeatedly to see queue operation
--- passport-vision/vision.js
+++ passport-vision/vision.js
@@ -7,11 +7,6 @@ const app = express()
 
 const identityDocumentQueue = new Queue('identityDocument')
 
-//some constants
-const STATUS_VALID = "valid";
-const STATUS_INVALID = "invalid";
-const STATUS_ERROR = "error";
-
 
 app.get('/', (req, res) => {
     const documentJob = identityDocumentQueue.createJob({ userId: "1342132154132", documentKey: "passport1.jpg" })
@@ -34,35 +29,39 @@ identityDocumentQueue.process(function (job, done) {
     
     var stdout = require('child_process').execSync(`mrz --json ${filePath}`);
     console.log(`[CV] Processing Task Completed.`)
-    console.log(stdout);
-    var matchedPassport = JSON.parse(stdout)
-    if  (matchedPassport.valid_score > 50) {
-        console.log("[CV] Valid document detected.")
-        console.log(matchedPassport);
-        var match = {};
-        match.processed = new Date();
-        match.firstname = matchedPassport.names;
-        match.lastname = matchedPassport.surname;
-        //match.gender = matchedPassport.sex;
-        if(matchedPassport.valid_date_of_birth) match.dob = moment(matchedPassport.date_of_birth, "YYMMDD").toDate();
-        if(matchedPassport.valid_expiration_date) match.documentExpireDate = moment(matchedPassport.expiration_date, "YYMMDD").toDate();
-        match.documentId = matchedPassport.number;
-        console.log(match);
-
-        // We should update MongoDB and add a record match with the match object
-        // Expose this via API to the frontend and allow users to compare based on matched fields.
-        // Our Schema is:
-        // processed (timestamp of process) - Date
-        // firstname (first name of user) - String
-        // lastname (last name of user) - String
-        // documentId (id on document) - String
-        // Date of Birth -- String
-        // Date of expiration  -- String
-        // Gender (not supported now, leave out. Kept in object for option of future support.)
-        return done(null, STATUS_SUCCESS);
-    } else {
-        console.log("[CV] No valid document detected.")
-        return done(null, STATUS_INVALID);
+    try {
+        var matchedPassport = JSON.parse(stdout);
+        if  (matchedPassport.valid_score > 50) {
+            console.log("[CV] Valid document detected.")
+            console.log(matchedPassport);
+            var match = {};
+            match.processed = new Date();
+            match.firstname = matchedPassport.names;
+            match.lastname = matchedPassport.surname;
+            //match.gender = matchedPassport.sex;
+            if(matchedPassport.valid_date_of_birth) match.dob = moment(matchedPassport.date_of_birth, "YYMMDD").toDate();
+            if(matchedPassport.valid_expiration_date) match.documentExpireDate = moment(matchedPassport.expiration_date, "YYMMDD").toDate();
+            match.documentId = matchedPassport.number;
+            console.log(match);
+    
+            // We should update MongoDB and add a record match with the match object
+            // Expose this via API to the frontend and allow users to compare based on matched fields.
+            // Our Schema is:
+            // processed (timestamp of process) - Date
+            // firstname (first name of user) - String
+            // lastname (last name of user) - String
+            // documentId (id on document) - String
+            // Date of Birth -- String
+            // Date of expiration  -- String
+            // Gender (not supported now, leave out. Kept in object for option of future support.)
+            return done();
+        } else {
+            console.log("[CV] No valid document detected.")
+            return done();
+        }
+    } catch (error) {
+        console.log(error);
+        done();
     }
 
 });
