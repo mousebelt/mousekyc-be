4ea0d1dccd2ca8591518720b5a03e0b43a9e6c50
Ohta Yoshida <ohtayoshida8912@yahoo.com>
2018-07-08 22:42:24 -0700

    updated config to ssl

--- src/config/development.js
+++ src/config/development.js
@@ -1,7 +1,9 @@
 const config = {
-    demoMode: false,
-    API_KEY: 'apikey-1234567890',
-    frontendBaseUrl: 'http://localhost',
-    baseUrl: 'http://localhost:3000',
+  demoMode: true,
+  API_KEY: 'apikey-1234567890',
+  frontendBaseUrl: 'http://localhost',
+  baseUrl: 'http://localhost:3000',
+
+  db: 'mongodb://localhost:27017/kyc-db',
 }
 module.exports = config;
--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const  _ = require("lodash");
 const defaults = require("./default.js");
-const config = require("./" + (process.env.NODE_ENV || "development") + ".js");
+const config = require("./" + (process.env.NODE_ENV || "production") + ".js");
 
 module.exports = _.merge({}, defaults, config);
